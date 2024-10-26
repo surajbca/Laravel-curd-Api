a7851d212f818970895af4dd92ec63a522dd802a
surajbca <krsurajkumar7481@gmail.com>
2024-10-26 13:14:25 +0530

    first commit

---
+++ laravel-api/app/Http/Controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();  // Should return a collection of products
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+        ]);
+
+        return Product::create($request->all());
+    }
+
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json([
+            'message' => 'Product deleted successfully!'
+        ], 204);
+    }
+}
